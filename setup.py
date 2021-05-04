c1188d20355a44843f615d9cfe9c0e665bbf7acb
deepadmax <maximillian.strand@gmail.com>
2021-05-04 23:25:23 +0200

    added module and setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='twich',
+    url='https://github.com/deepadmax/twich',
+    author='Maximillian Strand',
+    author_email='maximillian.strand@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    version='0.1',
+    license='',
+    description='Twich is a small library for interfacing with the Twitch API.',
+    long_description=open('README.md').read(),
+)
