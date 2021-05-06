d1e51f3e78e5356ca3dd5ad30b82656d56e37312
deepadmax <maximillian.strand@gmail.com>
2021-05-06 21:00:18 +0200

    updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='maximillian.strand@gmail.com',
     packages=find_packages(),
     install_requires=['requests', 'flask', 'websocket-client'],
-    version='0.1',
+    version='1.0',
     license='',
     description='Twich is a small library for interfacing with the Twitch API.',
     long_description=open('README.md').read(),
